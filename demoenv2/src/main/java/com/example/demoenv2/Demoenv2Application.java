3982654ea033dccacc3f58c143074830017ced18
Bisco <alidero1605@gmail.com>
2024-05-20 08:59:15 +0200

    Initial commit

---
+++ demoenv2/src/main/java/com/example/demoenv2/Demoenv2Application.java
@@ -0,0 +1,13 @@
+package com.example.demoenv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoenv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoenv2Application.class, args);
+	}
+
+}
---
+++ demoenv2/src/test/java/com/example/demoenv2/Demoenv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoenv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoenv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
